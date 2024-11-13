e5e660fbca72651479eca131f8f1a99f1df74e3d
ishandhanani <ishandhanani@gmail.com>
2024-11-13 04:43:37 +0000

    update imports

--- shared/setup.py
+++ shared/setup.py
@@ -4,5 +4,5 @@ setup(
     name="shared",
     version="0.1",
     packages=find_packages(),
-    install_requires=["redis", "pydantic"],
+    install_requires=["redis", "pydantic", "httpx", "requests", "langchain-nvidia-ai-endpoints"],
 )
